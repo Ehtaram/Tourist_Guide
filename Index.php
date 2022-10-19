ecbc18a5af9e39b426f81bfe8147e9463e1368c6
Mahammad Ehtaramullah <104067574+Ehtaram@users.noreply.github.com>
2022-10-19 22:14:32 +0530

    Added index.php

---
+++ Index.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	 <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="Css/Style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  
+</head>
+<body>
+
+	<nav class="navbar navbar-expand-lg navnar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Blog</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        
+          
+        
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="Images/Img1.jpg" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Kashmir</h3>
+        <p>Welcome to Kashmir!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="Images/Img2.jpg" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Goa</h3>
+        <p>Welcome to Goa!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="Images/Img3.png" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Himachal Pradesh</h3>
+        <p>Welcome to Himachal Pradesh!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="Images/Img4.jpg" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Uttrakhand</h3>
+        <p>Welcome to Uttrakhand!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="Images/Img5.jpg" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Agra</h3>
+        <p>Welcome to Agra!</p>
+      </div>   
+    </div>
+
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<section class="my-5">
+<div class="py-5">
+<h3 class="text-center">About Us</h3>
+</div>
+
+<div class="container-fluid">
+	<div class="row">
+	<div class="col-lg-3 col-md-4 col-10">
+		<img src="Images/img6.jpg" class="img-fluid">
+	</div>
+
+	<div class="col-lg-6 col-md-6 col-12">
+		<h2>Kashmir</h2>
+		<p>Jammu & Kashmir, a piece of heaven on earth, is the 19th largest state in India and it is located mostly all in the Himalayan mountains. It is also the only state in India with a Muslim-majority population. J&K consists of three main regions – Jammu, the Kashmir Valley and Ladakh. Between the three, the state offers beautiful verdant mountains and valleys, religious shrines and remote mountains.</p>
+		<a href="about.php">Check more</a>
+	</div>
+	</div>
+</div>
+
+</div>
+
+<div class="container-fluid">
+	<div class="row">
+	<div class="col-lg-3 col-md-4 col-10">
+		<img src="Images/img7.jpg" class="img-fluid aboutimage">
+	</div>
+
+	<div class="col-lg-6 col-md-4 col-12">
+		<h2>Goa</h2>
+		<p>Goa is a state in western India with coastlines stretching along the Arabian Sea. Its long history as a Portuguese colony prior to 1961 is evident in its preserved 17th-century churches and the area’s tropical spice plantations. Goa is also known for its beaches, ranging from popular stretches at Baga and Palolem to those in laid-back fishing villages such as Agonda. The Administrative head of the Department is Shri. Ravi Dhawan, IAS, Secretary Tourism. The Department has zonal office in North Goa at Headquarters, Panaji and in South Goa at Margao.</p>
+		<a href="about1.php">Check more</a>
+	</div>
+	</div>
+</div>
+
+
+</div>
+
+<div class="container-fluid">
+	<div class="row">
+	<div class="col-lg-3 col-md-4 col-10">
+		<img src="Images/img3.png" class="img-fluid">
+	</div>
+
+	<div class="col-lg-6 col-md-6 col-12">
+		<h2>Himachal Pradesh</h2>
+		<p>Tourism in Himachal Pradesh relates to tourism in the Indian state of Himachal Pradesh. This is popularly renowned for its Himalayan landscapes and popular hill-stations. Many outdoor activities such as rock climbing, mountain biking, paragliding, ice-skating, trekking, rafting, and heli-skiing are popular tourist attractions in Himachal Pradesh.</p>
+		<a href="about2.php">Check more</a>
+	</div>
+	</div>
+</div>
+
+
+</div>
+
+<div class="container-fluid">
+	<div class="row">
+	<div class="col-lg-3 col-md-4 col-10">
+		<img src="Images/img4.jpg" class="img-fluid">
+	</div>
+
+	<div class="col-lg-6 col-md-6 col-12">
+		<h2>Uttrakhand</h2>
+		<p>Jammu & Kashmir, a piece of heaven on earth, is the 19th largest state in India and it is located mostly all in the Himalayan mountains. It is also the only state in India with a Muslim-majority population. J&K consists of three main regions – Jammu, the Kashmir Valley and Ladakh. Between the three, the state offers beautiful verdant mountains and valleys, </p>
+		<a href="about3.php">Check more</a>
+	</div>
+	</div>
+</div>
+
+
+</div>
+
+<div class="container-fluid">
+	<div class="row">
+	<div class="col-lg-3 col-md-4 col-10">
+		<img src="Images/img5.jpg" class="img-fluid">
+	</div>
+
+	<div class="col-lg-6 col-md-6 col-12">
+		<h2>Agra</h2>
+		<p>Jammu & Kashmir, a piece of heaven on earth, is the 19th largest state in India and it is located mostly all in the Himalayan mountains. It is also the only state in India with a Muslim-majority population. J&K consists of three main regions – Jammu, the Kashmir Valley and Ladakh. Between the three, the state offers beautifu.</p>
+		<a href="about4.php">Check more</a>
+	</div>
+	</div>
+</div>
+</section>
+
+
+<section class="my-5">
+<div class="py-5">
+<h3 class="text-center">Our Services</h3>
+</div>
+
+<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-4 col-md-4 col-12">
+					<div class="card">
+		  <img class="card-img-top" src="Images/Img5.jpg" alt="Card image">
+		  <div class="card-body">
+		    <h4 class="card-title">Nice Architecture</h4>
+		    <p class="card-text">Let's See Incredible Architecture</p>
+		    <a href="#" class="btn btn-primary">See Profile</a>
+		  </div>
+		</div>
+
+		</div>
+
+<div class="col-lg-4 col-md-4 col-12">
+					<div class="card">
+		  <img class="card-img-top" src="Images/Img2.jpg" alt="Card image">
+		  <div class="card-body">
+		    <h4 class="card-title">Nature Beauty</h4>
+		    <p class="card-text">Let's Enjoy Natural Beauty</p>
+		    <a href="#" class="btn btn-primary">See Profile</a>
+		  </div>
+		</div>
+
+		</div>
+		<div class="col-lg-4 col-md-4 col-12">
+					<div class="card">
+		  <img class="card-img-top" src="Images/Img4.jpg" alt="Card image">
+		  <div class="card-body">
+		    <h4 class="card-title">Nice Place</h4>
+		    <p class="card-text">Let's Roam Around The Lovely Place</p>
+		    <a href="#" class="btn btn-primary">See Profile</a>
+		  </div>
+		</div>
+		</div>
+	</div>
+</div>
+</section>
+
+	<section class="my-5">
+	<div class="py-5">
+	<h3 class="text-center">Gallery</h3>
+	</div>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="Images/Img1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="Images/Img1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="Images/Img1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="Images/Img1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="Images/Img1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="Images/Img1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="Images/Img1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="Images/Img1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="Images/Img1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="Images/Img1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="Images/Img1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="Images/Img1.jpg" class="img-fluid pb-4">
+			</div>
+		</div>
+	</div>
+	</section>
+
+
+<section class="my-5">
+	<div class="py-5">
+	<h3 class="text-center">Contact Us</h3>
+	</div>
+
+	<div class="w-50 m-auto">
+		<form action="userinfo.php" method="post">
+			<div class="form-group">
+				<label>Username</label>
+				<input type="text" name="user" autocomplete="off" class="form-control">
+			</div>
+
+			<div class="form-group">
+				<label>Email Id</label>
+				<input type="text" name="email" autocomplete="off" class="form-control">
+			</div>
+
+			<div class="form-group">
+				<label>Mobile No.</label>
+				<input type="text" name="mobile" autocomplete="off" class="form-control">
+			</div>
+
+			<div class="form-group">
+				<label>Comments</label>
+				<textarea class="form-control" name="Comment">
+				</textarea>
+			</div>
+			<button type="submit" class="btn btn-success">Submit</button>
+		</form>
+	</div>
+</section>
+
+<footer>
+<p class="p-3 bg-dark text-white text-center">@PerfectBlog</p>
+</footer>
+
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
